0947fe055190fcbe8b1103e352ec411a312f813f
spselenim <pawal.satish@gmail.com>
2024-03-23 11:19:02 +0530

    Third

--- MyF/src/test/java/com/stc/MyF/FirstTest.java
+++ MyF/src/test/java/com/stc/MyF/FirstTest.java
@@ -5,6 +5,6 @@ import org.testng.annotations.Test;
 public class FirstTest {
   @Test
   public void f() {
-	  System.out.println("230324Test11 1");
+	  System.out.println("230324Test11 12");
   }
 }
